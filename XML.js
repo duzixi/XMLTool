964e159f374e04152571c1ba4604d27a76c67353
杜子兮 <duzixi@gmail.com>
2017-09-15 13:28:31 +0800

    谷歌和Safari浏览器xmlDoc生成成功（xml字符编码改为utf-8）

--- XML.js
+++ XML.js
@@ -1,50 +1,41 @@
 
 function XML(filePath) {
-	alert("XML");
 	this.filePath = filePath;
 	
 	var xmlDoc;
 	var isOk;
- 	try{ //IE浏览器
+ 	try{ // IE浏览器
  		xmlDoc = new ActiveXObject("microsoft.XMLDOM");
  		xmlDoc.async = false;
  		isOk = xmlDoc.load(this.filePath);
- 		alert("第一种");
  	}catch(e){
- 		try{ //Firefox, Mozilla, Opera, 其他浏览器
+ 		try{ // Firefox, Mozilla, Opera, 其他浏览器
  			xmlDoc = document.implementation.createDocument("","",null);
  			xmlDoc.async = false;
  			isOk = xmlDoc.load(this.filePath);
- 			alert("第二种");
  		}catch(e){
- 			try{ // 谷歌浏览器
+ 			try{ // 谷歌,Safari 浏览器
  				var xmlhttp = new window.XMLHttpRequest();  
          		xmlhttp.open("GET",this.filePath,false);  
          		xmlhttp.send(null);  
+                // alert(xmlhttp.responseText);
          		xmlDoc = xmlhttp.responseXML;
          		isOk = true;
-         		alert("第三种");
      		} catch (e){  
      			isOk = false;
      		}  
  		}
  	}
- 	alert("isOk:" + isOk);
- 	//转载xml文件
+
  	if(isOk){
  		this.xmlDoc = xmlDoc;
- 		alert("xmlDoc:" + xmlDoc);
- 		this.root = xmlDoc.documentElement; // Safari出错
- 		alert("this.root = xmlDoc.documentElement;");
- 		alert("文件" + this.filePath + "载入成功");
+ 		this.root = xmlDoc.documentElement; 
  	} else {
  		alert("文件" + this.filePath + "载入失败");
  	}
- 	
 }
 
 XML.prototype.getRootName = function () {
-	alert(this.root.nodeName);
 	return this.root.nodeName;
 }
 
